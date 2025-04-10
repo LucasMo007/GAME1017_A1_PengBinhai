6d7c821081300b94e154c6164bf2c0464052e5f2
LucasMo007 <easoncoldplay2@gmail.com>
2025-04-10 10:22:39 -0400

    Singleton pattern and Singleton Scorekeeper

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -9,12 +9,19 @@ public class LevelManager : MonoBehaviour
 {    
     
     [SerializeField] float sceneLoadDelay=2.0f;
-    public void LoadGame()
+    ScoreKeeper scoreKeeper;
+     void Awake()
     {
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
+    }
+    public void LoadGame()
+    {  
+        scoreKeeper.ResetScore();
         SceneManager.LoadScene("Game");
     }
     public void LoadMainMenu()
     {
+         
         SceneManager.LoadScene("MainMenu");
     }
     public void LoadGameOver()
